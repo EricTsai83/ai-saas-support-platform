b3f84382e6b957038666a05447143a74e448ac82
Eric Tsai <eric492718@gmail.com>
2025-11-02 01:41:02 +0800

    feat: implement WidgetAuthScreen for user authentication; add contact session handling and update dependencies in pnpm-lock.yaml

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { WidgetFooter } from "@/modules/widget/ui/components/widget-footer";
-import { WidgetHeader } from "@/modules/widget/ui/components/widget-header";
+import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
 
 interface Props {
   organizationId: string;
@@ -10,14 +9,8 @@ interface Props {
 export const WidgetView = ({ organizationId }: Props) => {
   return (
     <main className="min-h-svh min-w-svw flex flex-col  w-full h-full overflow-hidden rounded-xl border bg-muted">
-      <WidgetHeader>
-        <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
-          <p className="text-3xl">Hi there! 👋</p>
-          <p className="text-lg">How can I help you today?</p>
-        </div>
-      </WidgetHeader>
-      <div className="flex flex-1">Widget View: {organizationId}</div>
-      <WidgetFooter />
+      <WidgetAuthScreen />
+      {/* <WidgetFooter /> */}
     </main>
   );
 };
