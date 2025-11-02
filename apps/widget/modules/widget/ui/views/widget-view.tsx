d84c2958760a9d0f771aa5b0182f4a95d3f44ad6
Eric Tsai <eric492718@gmail.com>
2025-11-02 17:15:16 +0800

    feat: add jotai for state management; implement widget state atoms and constants; update Providers component to include jotai Provider

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,16 +1,29 @@
 "use client";
 
 import { WidgetAuthScreen } from "@/modules/widget/ui/screens/widget-auth-screen";
+import { useAtomValue } from "jotai";
+import { screenAtom } from "@/modules/widget/atoms/widget-atioms";
 
 interface Props {
   organizationId: string;
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom);
+
+  const screenComponents = {
+    error: <p>TOTO: Error</p>,
+    loading: <p>TOTO: Loading</p>,
+    auth: <WidgetAuthScreen />,
+    voice: <p>TOTO: Voice</p>,
+    inbox: <p>TOTO: Inbox</p>,
+    selection: <p>TOTO: Selection</p>,
+    chat: <p>TOTO: Chat</p>,
+    contact: <p>TOTO: Contact</p>,
+  };
   return (
-    <main className="min-h-svh min-w-svw flex flex-col  w-full h-full overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
-      {/* <WidgetFooter /> */}
+    <main className="min-h-svh min-w-svw flex flex-col w-full h-full overflow-hidden rounded-xl border bg-muted">
+      {screenComponents[screen]}
     </main>
   );
 };
